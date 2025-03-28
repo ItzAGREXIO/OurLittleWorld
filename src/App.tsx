b4551e8063949346e5ee276691148ae826343469
AGREXIO <agrexioaman@Gmail.com>
2025-03-28 15:00:51 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // Router hata diya
 import EntryGame from './components/EntryGame';
 import QuizGate from './components/QuizGate';
 import Home from './components/Home';
